10908dab15ef4c6399357271e2534732f0d75a2a
Dans Domanevskis <dandomanevski@gmail.com>
2024-12-17 23:33:06 +0200

    lightswitch examples in section 2

---
+++ OOP_Python/LightSwitch_OOP.py
@@ -0,0 +1,14 @@
+# OOP Light Switch
+
+class LightSwitch():
+    def __init__(self):
+        self.switchIsOn = False
+
+    def turnOn(self):
+        self.switchIsOn = True
+
+    def TurnOff(self):
+        self.switchIsOn = False
+
+
+oLightSwitch = LightSwitch()
